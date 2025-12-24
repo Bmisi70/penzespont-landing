e17ecec2ea43228da84ae2151e9364ff973b20d6
Bmisi70 <benyomisi@icloud.com>
2025-12-24 17:23:00 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import IntroLandingV2 from './App.js';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <IntroLandingV2 />
+  </StrictMode>
+);
